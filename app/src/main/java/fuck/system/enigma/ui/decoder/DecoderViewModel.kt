aaf103ec82866d6da9a955d1d133f5be992b228b
RokkyStudio <rokkystudio@gmail.com>
2025-07-08 19:42:51 +0300

    r.1.0.1

---
+++ app/src/main/java/fuck/system/enigma/ui/decoder/DecoderViewModel.kt
@@ -0,0 +1,15 @@
+package fuck.system.enigma.ui.decoder
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DecoderViewModel : ViewModel() {
+    val inputText = MutableLiveData<String>()
+    val key = MutableLiveData<String>()
+    val mode = MutableLiveData<String>()
+    val useBase64 = MutableLiveData<Boolean>().apply { value = true }
+    val userIv = MutableLiveData<Boolean>().apply { value = false }
+    val ivHex = MutableLiveData<String>()
+    val result = MutableLiveData<String>()
+    val isResultVisible = MutableLiveData<Boolean>().apply { value = false }
+}
